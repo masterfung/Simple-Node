c5d27f9adaefb386188556df1fafdeaa1553d364
Tsung Hung <hungmasterj@gmail.com>
2015-05-16 11:08:24 -0700

    updated with saveContacts and saveContacts to write files to the data.json

--- contact.js
+++ contact.js
@@ -2,17 +2,44 @@ var Contact = {
 	parseName: function(str) {
 	    var name = str.split(',');
 	    return name[0].trim();
-    },
-    	parseNumber: function(str) {
-            var num = str.split(',')[1].trim();
-	    return num;
-    },
-        createContact: function(str) {
-            return {
-		name: this.parseName(str),
-		number: this.parseNumber(str)
-        }
-    }
-};
+  },
+
+	parseNumber: function(str) {
+      var num = str.split(',')[1].trim();
+    return num;
+  },
+
+	createContact: function(str) {
+      return {
+				name: this.parseName(str),
+				number: this.parseNumber(str)
+			}
+
+  },
+
+	loadContacts: function(done){
+		var jsonfile = require('jsonfile');
+		jsonfile.readFile('data.json', function(err, data) {
+			done(err, data)
+		})
+
+	},
+
+	saveContacts: function(contacts, done) {
+		var jsonfile = require('jsonfile');
+		jsonfile.writeFile('data.json', contacts, function (err, data) {
+			done(err, data)
+		})
+	},
+
+	saveContact: function(contact, done) {
+		this.loadContacts(function(err, contacts) {
+			if (err) {return done(err)}
+			contacts.push(contact)
+			this.saveContacts(contacts, done)
+		}.bind(this))
+
+	}
+}
 
 module.exports = Contact;
--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var MyParrot = require('./parrot');
 var Contact = require('./contact');
 
+var contacts = [{name: "Jane Doe", number: "876-900-9898"}]
+var contact = { name: 'John Boon', number: '604-123-9090' };
+
 console.log(MyParrot.speakEnglish());
 console.log(MyParrot.speakSpanish());
 
@@ -8,3 +11,15 @@ console.log(Contact.parseName("John Smith,604-123-9090"));
 console.log(Contact.parseNumber("John Smith,604-123-9090"));
 
 console.log(Contact.createContact("John Smith,604-123-9090"));
+
+Contact.loadContacts(function(err, data) {
+  console.log(data);
+});
+
+Contact.saveContacts(contacts, function (err) {
+  console.log('Success!')
+})
+
+Contact.saveContact(contact, function(err) {
+  console.log('Wonderfully written sir!')
+})
