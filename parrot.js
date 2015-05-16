201b11d3d9b243c7a2ea00eb1053cec3ec31932f
Tsung Hung <hungmasterj@gmail.com>
2015-05-16 01:11:06 -0700

    first commit, a simple project of node

---
+++ parrot.js
@@ -0,0 +1,8 @@
+var Parrot = {
+	speakEnglish: function() {
+		return 'Hello!'},
+	speakSpanish: function() {
+		return 'Ola!'}
+};
+
+module.exports =  Parrot
---
+++ test.js
@@ -0,0 +1,4 @@
+var MyParrot = require('./parrot');
+
+console.log(MyParrot.speakEnglish())
+console.log(MyParrot.speakSpanish())
